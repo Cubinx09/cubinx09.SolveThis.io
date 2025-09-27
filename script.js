01e09e000b163a7c7003b6fcfd4fb7d10952b395
Cubinx09 <lalaneamanda0923@gmail.com>
2025-09-26 23:00:49 -0400

    script function works

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-const button = document.getElementsByTagName("button")[3]
-const a = document.getElementById("a")
-const c = document.getElementById("b")
-
-function check(){
-    if(button === a){
-        alert("Your answer is....CORRECT!")
-    } else if(button === b){
-        alert("Funny answer but.... INCORRECT!")
-    } else{
-        alert("Your answer is...INCORRECT!")
-    }
+function correct(){
+    alert("Your answer is.... CORRECT!!")
+}
+function incorrect(){
+    alert("Your answer is.... INCORRECT!!")
+}
+function funny(){
+    alert("Funny answer but it is.... INCORRECT!!")
 }
