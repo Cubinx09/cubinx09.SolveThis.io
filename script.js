12c4a8ea538bd70957d01873bd1cc469bb8061d9
Cubinx09 <lalaneamanda0923@gmail.com>
2025-09-28 22:10:27 -0400

    Trying to make popup boxes to mixed results.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function correct(){
-    alert("Your answer is.... CORRECT!!")
-}
-function incorrect(){
-    alert("Your answer is.... INCORRECT!!")
-}
-function funny(){
-    alert("Funny answer but it is.... INCORRECT!!")
-}
-function what(){
-    alert("Fair.")
-}
+
+//function correct(){
+    //alert("Your answer is.... CORRECT!!")
+//}
+//function incorrect(){
+    //alert("Your answer is.... INCORRECT!!")
+//}
+//function funny(){
+    //alert("Funny answer but it is.... INCORRECT!!")
+//}
+//function what(){
+    //alert("Fair.")
+//}
