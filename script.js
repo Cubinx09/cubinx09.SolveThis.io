d9433f43aad9b8f073c9c4c4785b24be4305a378
Cubinx09 <lalaneamanda0923@gmail.com>
2025-09-29 23:05:55 -0400

    Popup boxes work (now to fully customize and implement them)

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+let modal = document.getElementById("myModal")
+let btn = document.getElementById('open')
+let span = document.getElementById('close')
 
+btn.onclick = function(){
+    modal.style.display = 'block';
+}
+span.onclick = function(){
+    modal.style.display = 'none';
+}
+window.onclick = function(event){
+    if(event.target == modal){
+        modal.style.display = 'none';
+    }
+}
 //function correct(){
     //alert("Your answer is.... CORRECT!!")
 //}
