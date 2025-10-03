cab49a2738e417b944c5d26f3622275bba32c57b
Cubinx09 <lalaneamanda0923@gmail.com>
2025-10-02 20:50:21 -0400

    that's a lot of variables...but this is the best i can do.

--- script.js
+++ script.js
@@ -1,27 +1,70 @@
 let modal = document.getElementById("myModal")
-let btn = document.getElementById('open')
+let modal2 = document.getElementById("yourModal")
+let modal3 = document.getElementById("ourModal")
+
+let btn = document.getElementById('1')
+let button = document.getElementById("2")
+let boton = document.getElementById('3')
+let pushy = document.getElementById('4')
+let buton = document.getElementById('5')
+let putton = document.getElementById('6')
+let buttoN = document.getElementById('7')
+let pusher = document.getElementById('8')
+let mutton = document.getElementById('9')
+let qutton = document.getElementById('10')
+let futton = document.getElementById('11')
+let vutton = document.getElementById('12')
+
 let span = document.getElementById('close')
+let span2 = document.getElementById('cerrar')
+let span3 = document.getElementById('shoo')
 
 btn.onclick = function(){
+    modal.style.display = 'block';
+}
+buton.onclick = function(){
+    modal.style.display = 'block';
+}
+qutton.oinclick = function(){
     modal.style.display = 'block';
 }
 span.onclick = function(){
     modal.style.display = 'none';
 }
-window.onclick = function(event){
-    if(event.target == modal){
-        modal.style.display = 'none';
-    }
+
+button.onclick = function(){
+    modal2.style.display = 'block'
 }
-//function correct(){
-    //alert("Your answer is.... CORRECT!!")
-//}
-//function incorrect(){
-    //alert("Your answer is.... INCORRECT!!")
-//}
-//function funny(){
-    //alert("Funny answer but it is.... INCORRECT!!")
-//}
-//function what(){
-    //alert("Fair.")
-//}
+boton.onclick = function(){
+    modal2.style.display = 'block'
+}
+putton.onclick = function(){
+    modal2.style.display = 'block';
+}
+buttoN.onclick = function(){
+    modal2.style.display = 'block';
+}
+mutton.onclick = function(){
+    modal2.style.display = 'block';
+}
+futton.onclick = function(){
+    modal2.style.display = 'block';
+}
+span2.onclick = function(){
+    modal2.style.display = 'none'
+}
+
+pushy.onclick = function(){
+    modal3.style.display = 'block'
+}
+pusher.onclick = function(){
+    modal3.style.display = 'block'
+}
+vutton.onclick = function(){
+    modal3.style.display = 'block';
+}
+span3.onclick = function(){
+    modal3.style.display = 'none'
+}
+
+
