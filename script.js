daf9088c4c55fabd7dbfdc445b46ad01370f37f2
Cubinx09 <lalaneamanda0923@gmail.com>
2025-10-04 01:41:06 -0400

    time to customize!

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let modal = document.getElementById("myModal")
 let modal2 = document.getElementById("yourModal")
 let modal3 = document.getElementById("ourModal")
+let modal4 = document.getElementById('weModal')
 
 let btn = document.getElementById('1')
 let button = document.getElementById("2")
@@ -14,10 +15,15 @@ let mutton = document.getElementById('9')
 let qutton = document.getElementById('10')
 let futton = document.getElementById('11')
 let vutton = document.getElementById('12')
+let yutton = document.getElementById('13')
+let rutton = document.getElementById('14')
+let zutton = document.getElementById('15')
+let tutton = document.getElementById("16")
 
 let span = document.getElementById('close')
 let span2 = document.getElementById('cerrar')
 let span3 = document.getElementById('shoo')
+let span4 = document.getElementById('goAway')
 
 btn.onclick = function(){
     modal.style.display = 'block';
@@ -28,15 +34,18 @@ buton.onclick = function(){
 qutton.oinclick = function(){
     modal.style.display = 'block';
 }
+zutton.onclick = function(){
+    modal.style.display = 'block';
+}
 span.onclick = function(){
     modal.style.display = 'none';
 }
 
 button.onclick = function(){
-    modal2.style.display = 'block'
+    modal2.style.display = 'block';
 }
 boton.onclick = function(){
-    modal2.style.display = 'block'
+    modal2.style.display = 'block';
 }
 putton.onclick = function(){
     modal2.style.display = 'block';
@@ -50,21 +59,32 @@ mutton.onclick = function(){
 futton.onclick = function(){
     modal2.style.display = 'block';
 }
+yutton.onclick = function(){
+    modal2.style.display = 'block';
+}
+rutton.onclick = function(){
+    modal2.style.display = 'block';
+}
 span2.onclick = function(){
-    modal2.style.display = 'none'
+    modal2.style.display = 'none';
 }
 
 pushy.onclick = function(){
-    modal3.style.display = 'block'
+    modal3.style.display = 'block';
 }
 pusher.onclick = function(){
-    modal3.style.display = 'block'
+    modal3.style.display = 'block';
 }
 vutton.onclick = function(){
     modal3.style.display = 'block';
 }
 span3.onclick = function(){
-    modal3.style.display = 'none'
+    modal3.style.display = 'none';
 }
 
-
+tutton.onclick = function(){
+    modal4.style.display = 'block';
+}
+span4.onclick = function(){
+    modal4.style.display = 'none';
+}
